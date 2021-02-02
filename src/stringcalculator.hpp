2a3a75c967d0836b6d410dc799aa9b92674b7538
Lawtonlegacy <rjlawton@alaska.edu>
2021-02-01 18:30:13 -0800

    Cleaned up repo and passed comma delimited test.

--- src/stringcalculator.hpp
+++ src/stringcalculator.hpp
@@ -1,7 +1,19 @@
 #ifndef STRINGCALCULATOR_HPP
 #define STRINGCALCULATOR_HPP
 #include <string>
+#include <sstream>
 
-int Add(std::string numbers);
+int Add(std::string numbers){
+    std::istringstream ss(numbers);
+    int sum = 0;
+    std::string token;
+    if(numbers.empty()) return 0; // return 0 on empty string.
+
+    while(std::getline(ss, token, ',')){
+        sum+= std::stoi(token);
+    }
+    
+    return sum;
+}
 
 #endif
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,27 +1,19 @@
 #include "catch.hpp"
 #include "stringcalculator.hpp"
-#include <string>
 
-int Add(std::string numbers){
-    int number;
-    if(numbers.empty()) return 0;
-    number = std::stoi(numbers);
-    return number;
-}
 
 TEST_CASE( "Empty string returns 0"){
     REQUIRE( Add("") == 0 );
 }
 
 TEST_CASE("Single number returns the value"){
-    
-    std::string test = "5";
-    REQUIRE( Add(test) == 5 );
-    REQUIRE( Add("6") == 6 );
+    REQUIRE( Add("5") == 5 );
+    REQUIRE( Add("20") == 20 );
+    REQUIRE( Add("300") == 300 );
 }
 
 TEST_CASE("Two numbers, comma delimted, returns the sum"){
-    std::string test = "1 + 1";
-    REQUIRE( Add(test) == 2);
-    REQUIRE( Add("1 + 1") == 2);
+    REQUIRE( Add("1,1") == 2);
+    REQUIRE( Add("5 , 12") == 17);
+    REQUIRE( Add("1000 , 534") == 1534);
 }
