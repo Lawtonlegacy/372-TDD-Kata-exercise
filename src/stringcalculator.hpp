801a4dee1c2cf4abd9fd7106f8110b0909ee63d2
Lawtonlegacy <rjlawton@alaska.edu>
2021-02-01 18:57:53 -0800

    Refactored and newline delimited test case passed.

--- src/stringcalculator.hpp
+++ src/stringcalculator.hpp
@@ -2,12 +2,16 @@
 #define STRINGCALCULATOR_HPP
 #include <string>               // std::string, std::stoi, std::getline
 #include <sstream>              // std::istringstream
+#include <algorithm>            // std::replace
 
 int Add(std::string numbers){
+    if(numbers.empty()) return 0; // return 0 on empty string.
+    
+    std::replace(numbers.begin(), numbers.end(), '\n', ','); // replaces newlines with commas
     std::istringstream ss(numbers);
+   
     int sum = 0;
     std::string token;
-    if(numbers.empty()) return 0; // return 0 on empty string.
 
     while(std::getline(ss, token, ',')){
         sum+= std::stoi(token);
